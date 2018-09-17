07024d87c68d7f79ee823ce816e3bcbd877529ea
oksana richards <oksana_vldk@hotmail.com>
2018-09-17 10:24:31 +0100

    add pet to customer

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -159,12 +159,12 @@ class TestPetShop < Minitest::Test
     assert_equal(0, count)
   end
   #
-  # def test_add_pet_to_customer
-  #   customer = @customers[0]
-  #   add_pet_to_customer(customer, @new_pet)
-  #   assert_equal(1, customer_pet_count(customer))
-  # end
-  #
+  def test_add_pet_to_customer
+    customer = @customers[0]
+    add_pet_to_customer(customer, @new_pet)
+    assert_equal(1, customer_pet_count(customer))
+  end
+  
   # # # OPTIONAL
   #
   # def test_customer_can_afford_pet__insufficient_funds
