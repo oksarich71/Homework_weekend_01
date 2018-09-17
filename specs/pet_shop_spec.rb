d5ae745d6a99546aa81e5b56b083771046441815
oksana richards <oksana_vldk@hotmail.com>
2018-09-17 10:25:28 +0100

    customer can afford pet

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -164,14 +164,14 @@ class TestPetShop < Minitest::Test
     add_pet_to_customer(customer, @new_pet)
     assert_equal(1, customer_pet_count(customer))
   end
-  
+
   # # # OPTIONAL
   #
-  # def test_customer_can_afford_pet__insufficient_funds
-  #   customer = @customers[1]
-  #   can_buy_pet = customer_can_afford_pet(customer, @new_pet)
-  #   assert_equal(false, can_buy_pet)
-  # end
+  def test_customer_can_afford_pet__insufficient_funds
+    customer = @customers[1]
+    can_buy_pet = customer_can_afford_pet(customer, @new_pet)
+    assert_equal(false, can_buy_pet)
+  end
   #
   # def test_customer_can_afford_pet__sufficient_funds
   #   customer = @customers[0]
